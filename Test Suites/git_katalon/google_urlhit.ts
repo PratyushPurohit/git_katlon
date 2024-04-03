59d532b6fb0887161a0506610e54700574067d67
PratyushPurohit <pratyushpurohit8888@gmail.com>
2024-04-03 12:16:42 +0530

    new pratice

---
+++ Test Suites/git_katalon/google_urlhit.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>google_urlhit</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>7fd7bb69-2c33-4b93-9b73-ca296fbfb405</testSuiteGuid>
+   <testCaseLink>
+      <guid>7e9be08e-0ec2-4c4c-bf5b-6fc825a0d09d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/git_katlon/google_git_demo</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
